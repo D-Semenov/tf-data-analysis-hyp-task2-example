566d99bfc0b0ae67a08958592b388c4f0ef98d14
D-Semenov <129876525+D-Semenov@users.noreply.github.com>
2023-04-10 00:24:11 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1474426447 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
+    import scipy.stats as sts
+    stat, p = sts.ttest_ind(x, y)
+    alpha = 0.03
+    answer = p < alpha
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return answer # Ваш ответ, True или False
